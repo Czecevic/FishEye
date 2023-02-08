b37b89bff637e924e54060a8f2e9928b972eb07f
Charles <charleszecevic@gmail.com>
2023-02-08 12:43:38 +0100

    correct erreur nulle

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -5,13 +5,6 @@ async function getData() {
   return data;
 }
 
-async function getData() {
-  const data = await fetch("./data/photographers.json")
-    .then((result) => result.json())
-    .catch((error) => error);
-  return data;
-}
-
 function displayData(photographers) {
   console.log(photographers);
   const photographersSection = document.querySelector(".photographer_section");
@@ -22,15 +15,6 @@ function displayData(photographers) {
     photographersSection.innerHTML += userCardDOM;
   });
 }
-
-async function init() {
-  // Récupère les datas des photographes
-  const { photographers } = await getData();
-  console.log(photographers);
-  displayData(photographers);
-}
-
-init();
 
 async function init() {
   // Récupère les datas des photographes
